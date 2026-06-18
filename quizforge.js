cdcba1da71484066c2d7f1a0ca06b183e6acbc76
matikoxfirefox <ffox77343@gmail.com>
2026-06-18 16:26:14 +0200

    First commit

---
+++ quizforge.js
@@ -0,0 +1,11 @@
+// async function TestQuizRes() {
+//     try {
+//         const response = await fetch('https://opentdb.com/api.php?amount=30');
+//         const jsonResponse = await response.json();
+//         console.log(jsonResponse);
+//     }
+//     catch(err) {
+//         console.log(err);
+//     }
+// }
+// TestQuizRes()
